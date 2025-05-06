2ea762d6f7a66ebfd72aa26b2c0bb251b4ca7772
root <root@DESKTOP-SQPUCEH>
2025-05-06 19:25:07 +0300

    Refactor OrderProduct and ProductSize models
    - Removed ProductId from OrderProduct and added ProductSizeId.
    - Updated navigation property from Product to ProductSize.
    - Added StockQuantity to ProductSize and removed Size navigation property.

--- ShopDomain/Model/OrderProduct.cs
+++ ShopDomain/Model/OrderProduct.cs
@@ -6,12 +6,9 @@ namespace ShopDomain.Model;
 public partial class OrderProduct : Entity
 {
     public int OrderId { get; set; }
-
-    public int ProductId { get; set; }
-
+    public int ProductSizeId { get; set; } // Змінюємо на ProductSizeId
     public int Quantity { get; set; }
 
     public virtual Order Order { get; set; } = null!;
-
-    public virtual Product Product { get; set; } = null!;
+    public virtual ProductSize ProductSize { get; set; } = null!; // Змінюємо на ProductSize
 }
--- ShopDomain/Model/ProductSize.cs
+++ ShopDomain/Model/ProductSize.cs
@@ -6,10 +6,9 @@ namespace ShopDomain.Model;
 public partial class ProductSize : Entity
 {
     public int ProductId { get; set; }
-
     public int SizeId { get; set; }
+    public int StockQuantity { get; set; } // Додаємо для запасів
 
     public virtual Product Product { get; set; } = null!;
-
     public virtual Size Size { get; set; } = null!;
 }
